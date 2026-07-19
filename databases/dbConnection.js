aa20ddd9abed6f8345e722a142604b2b1db0450b
gamal1102000-maker <gamal1102000@gmail.com>
2026-07-19 15:42:25 +0300

    update name database

--- databases/dbConnection.js
+++ databases/dbConnection.js
@@ -2,7 +2,7 @@
 import mongoose from 'mongoose'
 
 export const dbconnection = () => {
-    mongoose.connect(process.env.DB_CONNECTION)
+    mongoose.connect(process.env.BASE_URL)
         .then(() => {
             console.log('database is running....')
         }).catch((err) => {
