8482ae5c3d7f65cbda49c1aaa4fe6be8f54c7e2e
gamal1102000-maker <gamal1102000@gmail.com>
2026-07-19 16:21:52 +0300

    fix: update db connection variable to DB_ONLINE

--- databases/dbConnection.js
+++ databases/dbConnection.js
@@ -3,13 +3,11 @@ import mongoose from 'mongoose'
 
 export const dbconnection = () => {
     mongoose.connect(process.env.DB_ONLINE)
-
-    const dbUrl = process.env.DB_ONLINE || process.env.DB_CONNECTION;
-    mongoose.connect(dbUrl)
         .then(() => {
             console.log('database is running....')
         }).catch((err) => {
-            console.log('ERRORRRR Database', err)
+            console.log('ERRORRRR Database',err)
         })
 }
+
 
