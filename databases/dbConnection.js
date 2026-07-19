9cbb7e9897fd5f187ba76cbcdd293829c43456b4
gamal1102000-maker <gamal1102000@gmail.com>
2026-07-19 16:26:53 +0300

    fix: make database connection dynamic for local and production

--- databases/dbConnection.js
+++ databases/dbConnection.js
@@ -1,13 +1,14 @@
 
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 export const dbconnection = () => {
-    mongoose.connect(process.env.DB_ONLINE)
+    const dbUrl = process.env.DB_ONLINE || process.env.DB_CONNECTION;
+
+    mongoose.connect(dbUrl)
         .then(() => {
             console.log('database is running....')
         }).catch((err) => {
-            console.log('ERRORRRR Database',err)
+            console.log('ERRORRRR Database', err)
         })
 }
-
 
